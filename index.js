9e7d7d20a644c45de674a3e5485b1c2d56177f86
Julian M. Qapo <julian.qapo@gmail.com>
2018-12-31 21:24:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,15 +11,17 @@ function produceDrivingRange(block){
 */
 
 
-function x(){
-  function y(i){
-    if (i>=10){
-      return false;
-    }else {
-      return true;
+function produceDrivingRange(blocks){
+  function z(a,b){
+    let c = Math.abs(a-b){
+      if (c>blocks){
+        return `${c} blocks out of range`
+      }
+      else {return `within range by ${c}`}
+        
+      }
+      }
     }
-    }
-return y
+  }
+  return z
 }
-
-let produceDrivingRange = x()
